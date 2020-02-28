821b7d9042e0a81d59f956d61c17ecc87e38ecb4
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-02-28 09:07:44 +0100

    Ajout de méthodes de récupération de données

--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -1,15 +1,21 @@
-import { initializeApp, firestore, storage } from 'firebase/app';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
 
-initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-})
+async function getDocumentsFromCollection<T>(collectionName: string): Promise<T[]> {
+    const documents: T[] = []
+    await firebase.firestore().collection(collectionName).get().then((snap: firebase.firestore.QuerySnapshot) => {
+        if (snap.empty) {
+            return;
+        }
+        snap.forEach(doc => {
+            const data = doc.data() as T;
+
+            documents.push(data);
+        });
+    });
+    return documents;
+}
 
 export {
-    firestore,
-    storage,
+    getDocumentsFromCollection,
 };
