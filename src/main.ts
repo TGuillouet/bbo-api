be0bf4aabe0368d2fde17dd9a5f41260b71f0de6
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-02-28 09:52:26 +0100

    Commentaires

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,6 @@ import { AppModule } from './app.module';
 import * as firebase from 'firebase-admin';
 
 async function bootstrap() {
-  // firebase.initializeApp({
-  //   apiKey: process.env.FIREBASE_API_KEY,
-  //   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  //   databaseURL: process.env.FIREBASE_DATABASE_URL,
-  //   projectId: process.env.FIREBASE_PROJECT_ID,
-  //   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  //   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  // })
   firebase.initializeApp({
     credential: firebase.credential.cert(process.env.FIREBASE_CREDENTIALS_PATH),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
