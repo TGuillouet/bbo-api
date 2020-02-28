d05cdc469cf2856ba265c0f56ee38b311e198cdb
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-02-28 10:10:55 +0100

    fix middleware

--- src/middlewares/auth-middleware.service.ts
+++ src/middlewares/auth-middleware.service.ts
@@ -6,15 +6,16 @@ export class AuthMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     const [ type, token ] = req.headers.authorization.split(' ');
     if (type && type === 'Bearer' && token) {
-      firebase.auth().verifyIdToken(token)
+        firebase.auth().verifyIdToken(token)
           .then((decodedToken) => {
-            res.locales.uid = decodedToken.uid;
+            res.locals.uid = decodedToken.uid;
             next();
           })
           .catch(error => {
-            throw new HttpException('You token is not valid', HttpStatus.FORBIDDEN);
+            res.status(403).json({ message: 'You token is not valid', status: HttpStatus.FORBIDDEN });
           });
+    } else {
+        throw new HttpException('Token is missing', HttpStatus.UNAUTHORIZED);
     }
-    throw new HttpException('Token is missing', HttpStatus.UNAUTHORIZED);
   }
 }
